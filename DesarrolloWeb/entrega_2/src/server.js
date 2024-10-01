99b1a35e760061d8524e1a46046b05e61b0b2aff
Thelilsaints <ever.cordoba956@pascualbravo.edu.co>
2024-10-01 14:38:00 -0500

    para trabajar la funcionalidad de la web

---
+++ DesarrolloWeb/entrega_2/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("hola mundo")
